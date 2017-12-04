a2876e9f1c1c46ee8d92e8c1141bb9ae1de773eb
Stephan Florquin <stephan.florquin@gmail.com>
2017-12-04 21:59:47 +0100

    cli/script: add libclosure

---
+++ cli/script/libclosure.go
@@ -0,0 +1,71 @@
+// Copyright © 2017  Stratumn SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package script
+
+// LibClosure contains functions to work with closures.
+var LibClosure = map[string]InterpreterFuncHandler{
+	"let": LibClosureLet,
+}
+
+// LibClosureLet binds a value to a symbol in the current closure.
+func LibClosureLet(ctx *InterpreterContext) (SExp, error) {
+	// Get:
+	//
+	//	1. car symbol value
+	if ctx.Args == nil {
+		return nil, ctx.Error("missing symbol")
+	}
+
+	car := ctx.Args.Car()
+	if ctx.Args == nil {
+		return nil, ctx.Error("missing symbol")
+	}
+
+	carSymbol, ok := car.SymbolVal()
+	if !ok {
+		return nil, ctx.Error("not a symbol")
+	}
+
+	//	2. cadr, optional (value)
+	cdr := ctx.Args.Cdr()
+	var cadr SExp
+
+	if cdr != nil {
+		cdrCell, ok := cdr.CellVal()
+		if !ok {
+			return nil, ctx.Error("invalid value")
+		}
+
+		cadr = cdrCell.Car()
+	}
+
+	//	3. a value isn't already bound to symbol in the current
+	//	   closure
+
+	if _, ok := ctx.Closure.Local("$" + carSymbol); ok {
+		return nil, ctx.Error("a value is already bound to the symbol")
+	}
+
+	// Evaluate cadr (the value).
+	v, err := ctx.Eval(ctx.Ctx, ctx.Closure, cadr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Bind the value to the symbol and return the value.
+	ctx.Closure.SetLocal("$"+carSymbol, v)
+
+	return v, nil
+}
---
+++ cli/script/libclosure_test.go
@@ -0,0 +1,45 @@
+// Copyright © 2017  Stratumn SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package script
+
+import (
+	"testing"
+)
+
+var libClosureTests = []libTest{{
+	"let a 'hello'",
+	`"hello"`,
+	"",
+}, {
+	"let a",
+	"",
+	"",
+}, {
+	"let",
+	"",
+	"1:1: let: missing symbol",
+}, {
+	"let 'a'",
+	"",
+	"1:1: let: not a symbol",
+}, {
+	"let a 'hello'\nlet a 'world'",
+	"",
+	"2:1: let: a value is already bound to the symbol",
+}}
+
+func TestLibClosure(t *testing.T) {
+	testLib(t, LibClosure, libClosureTests)
+}

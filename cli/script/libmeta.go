6219c9cb3743797960fa88f4f5c7bf9d6f8bf757
Stephan Florquin <stephan.florquin@gmail.com>
2017-12-04 20:24:23 +0100

    cli/script: add libmeta

---
+++ cli/script/libmeta.go
@@ -0,0 +1,52 @@
+// Copyright © 2017  Stratumn SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package script
+
+// LibMeta contains functions to quote and evaluate expressions.
+var LibMeta = map[string]InterpreterFuncHandler{
+	"quote": LibMetaQuote,
+	"eval":  LibMetaEval,
+}
+
+// LibMetaQuote returns an expression without evaluating it.
+func LibMetaQuote(ctx *InterpreterContext) (SExp, error) {
+	// Quote is the simplest command, it just returns the car as is.
+
+	if ctx.Args == nil || ctx.Args.Cdr() != nil {
+		return nil, ctx.Error("expected a single expression")
+	}
+
+	return ctx.Args.Car(), nil
+}
+
+// LibMetaEval evauluates an expression.
+func LibMetaEval(ctx *InterpreterContext) (SExp, error) {
+	// Eval is a simple command, all it does is evaluate the car twice.
+	if ctx.Args == nil || ctx.Args.Cdr() != nil {
+		return nil, ctx.Error("expected a single expression")
+	}
+
+	v, err := ctx.Eval(ctx.Ctx, ctx.Closure, ctx.Args.Car())
+	if err != nil {
+		return nil, ctx.WrapError(err)
+	}
+
+	v, err = ctx.Eval(ctx.Ctx, ctx.Closure, v)
+	if err != nil {
+		return nil, ctx.WrapError(err)
+	}
+
+	return v, nil
+}
---
+++ cli/script/libmeta_test.go
@@ -0,0 +1,73 @@
+// Copyright © 2017  Stratumn SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package script
+
+import (
+	"testing"
+)
+
+var libMetaTests = []libTest{{
+	"quote hello",
+	"hello",
+	"",
+}, {
+	"quote (hello)",
+	"(hello)",
+	"",
+}, {
+	"quote (hello world)",
+	"(hello world)",
+	"",
+}, {
+	"quote (quote (quote hello))",
+	"(quote (quote hello))",
+	"",
+}, {
+	"quote",
+	"",
+	"1:1: quote: expected a single expression",
+}, {
+	"quote hello world",
+	"",
+	"1:1: quote: expected a single expression",
+}, {
+	"eval 'hello world'",
+	`"hello world"`,
+	"",
+}, {
+	"eval (quote (quote 'hello world'))",
+	`"hello world"`,
+	"",
+}, {
+	"eval",
+	"",
+	"1:1: eval: expected a single expression",
+}, {
+	"eval a",
+	"",
+	"1:1: eval: a: could not resolve symbol",
+}, {
+	"eval a b",
+	"",
+	"1:1: eval: expected a single expression",
+}, {
+	"eval (quote ('hello world'))",
+	``,
+	`1:1: eval: 1:14: "hello world": function name is not a symbol`,
+}}
+
+func TestLibMeta(t *testing.T) {
+	testLib(t, LibMeta, libMetaTests)
+}

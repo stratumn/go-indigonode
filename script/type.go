3fe0c45d9a8fd6d0210ae7accc1b725295998825
Stephan Florquin <stephan.florquin@gmail.com>
2017-12-10 03:33:24 +0100

    script: add more checks to type checker

--- script/type.go
+++ script/type.go
@@ -27,6 +27,24 @@ var (
 	// ErrTypeExtraArg is emitted when a function call has too many
 	// arguments.
 	ErrTypeExtraArg = errors.New("extra function argument")
+
+	// ErrCarNil is emitted when the car of a cell is nil.
+	ErrCarNil = errors.New("car cannot be nil")
+
+	// ErrNotSymbol is emitted when a symbol is expected.
+	ErrNotSymbol = errors.New("not a symbol")
+
+	// ErrNotCell is emitted when a cell is expected.
+	ErrNotCell = errors.New("not a cell")
+
+	// ErrNil is emitted when a value cannot be nil.
+	ErrNil = errors.New("value cannot be nil")
+
+	// ErrBound is emitted when a symbol is already locally bound.
+	ErrBound = errors.New("symbol is already bound locally")
+
+	// ErrNotBound is emitted when a symbol is not bound to a value.
+	ErrNotBound = errors.New("symbol is not bound to a value")
 )
 
 // Type is an SExp type.
@@ -35,6 +53,8 @@ type Type uint8
 // Expression types.
 const (
 	TypeInvalid Type = iota
+	TypeNil
+	TypeAny
 	TypeString
 	TypeInt64
 	TypeBool
@@ -46,6 +66,8 @@ const (
 // Maps types to their names.
 var typeMap = map[Type]string{
 	TypeInvalid:  "invalid",
+	TypeNil:      "nil",
+	TypeAny:      "any",
 	TypeString:   "string",
 	TypeInt64:    "int64",
 	TypeBool:     "bool",
@@ -61,16 +83,108 @@ func (t Type) String() string {
 
 // TypeInfo contains information about a type.
 type TypeInfo struct {
-	Type     Type
-	IsQuoted bool
+	Type    Type
+	IsList  bool
+	CarType *TypeInfo
+	CdrType *TypeInfo // Nil when list
+}
+
+// String returns a string representation of the type info.
+func (ti *TypeInfo) String() string {
+	s := ""
+
+	switch {
+	case ti.Type == TypeCell && ti.IsList:
+		s = "list<" + ti.CarType.String() + ">"
+	case ti.Type == TypeCell:
+		s = "pair<"
+		if ti.CarType == nil {
+			s += "nil"
+		} else {
+			s += ti.CarType.String()
+		}
+		s += ","
+		if ti.CdrType == nil {
+			s += "nil"
+		} else {
+			s += ti.CdrType.String()
+		}
+		s += ">"
+	default:
+		s = ti.Type.String()
+	}
+
+	return s
+}
+
+// Equals returns whether the type is the same as another type.
+func (ti *TypeInfo) Equals(t *TypeInfo) bool {
+	if ti.Type != t.Type {
+		return false
+	}
+
+	if ti.IsList != t.IsList {
+		return false
+	}
+
+	if ti.IsList && !ti.CarType.Equals(t.CarType) {
+		return false
+	}
+
+	if ti.Type == TypeCell {
+		if !ti.CarType.Equals(t.CarType) {
+			return false
+		}
+
+		if !ti.CdrType.Equals(t.CdrType) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Compatible returns whether the given type is compatible with the type info.
+//
+// The order matters. For instance a receiver with type Any is compatible with
+// any type, but not the other way around. A type is only compatible with a
+// more specific type.
+func (ti *TypeInfo) Compatible(t *TypeInfo) bool {
+	if ti.Type == TypeAny {
+		return true
+	}
+
+	if ti.Type != t.Type {
+		return false
+	}
+
+	if ti.IsList != t.IsList {
+		return false
+	}
+
+	if ti.IsList && !ti.CarType.Compatible(t.CarType) {
+		return false
+	}
+
+	if ti.Type == TypeCell {
+		if !ti.CarType.Compatible(t.CarType) {
+			return false
+		}
+
+		if !ti.CdrType.Compatible(t.CdrType) {
+			return false
+		}
+	}
+
+	return true
 }
 
 // TypeError is a type error.
 type TypeError struct {
 	Line   int
 	Offset int
-	Got    Type
-	Want   Type
+	Got    *TypeInfo
+	Want   *TypeInfo
 }
 
 // Error retruns a string representation of the error.
@@ -80,6 +194,70 @@ func (e TypeError) Error() string {
 		e.Line, e.Offset,
 		e.Got, e.Want,
 	)
+}
+
+// TypeCheckerContext holds information about the context in wich a type is
+// being checked.
+type TypeCheckerContext struct {
+	locals    map[string]*TypeInfo
+	inherited map[string]*TypeInfo
+
+	// If an expression is known to be nil, isNil will be true. If it is
+	// known to not be nil, isNil will be false. If it is unknown, it won't
+	// have a value.
+	isNil map[uint64]bool
+}
+
+// NewTypeCheckerContext creates a new type checker context.
+//
+// Parent can be nil.
+func NewTypeCheckerContext(parent *TypeCheckerContext) *TypeCheckerContext {
+	ctx := &TypeCheckerContext{
+		locals:    map[string]*TypeInfo{},
+		inherited: map[string]*TypeInfo{},
+		isNil:     map[uint64]bool{},
+	}
+
+	if parent == nil {
+		return ctx
+	}
+
+	for k, v := range parent.inherited {
+		ctx.inherited[k] = v
+	}
+
+	for k, v := range parent.locals {
+		ctx.inherited[k] = v
+	}
+
+	for k, v := range parent.isNil {
+		ctx.isNil[k] = v
+	}
+
+	return ctx
+}
+
+// Set sets the type of a local symbol.
+func (ctx *TypeCheckerContext) Set(symbol string, ti *TypeInfo) {
+	ctx.locals[symbol] = ti
+}
+
+// Get returns the type of a local or inherited symbol.
+func (ctx *TypeCheckerContext) Get(symbol string) (*TypeInfo, bool) {
+	if ti, ok := ctx.locals[symbol]; ok {
+		return ti, true
+	}
+
+	ti, ok := ctx.inherited[symbol]
+
+	return ti, ok
+}
+
+// IsLocal retruns whether a value is locally bound to the symbol.
+func (ctx *TypeCheckerContext) IsLocal(symbol string) bool {
+	_, ok := ctx.locals[symbol]
+
+	return ok
 }
 
 // TypeCheckerOpt is a type checker option.
@@ -94,14 +272,16 @@ func TypeCheckerOptErrorHandler(h func(error)) TypeCheckerOpt {
 
 // TypeChecker type-checks S-Expressions.
 type TypeChecker struct {
-	cache map[string]TypeInfo
+	cache map[uint64]*TypeInfo
 
 	errHandler func(error)
 }
 
 // NewTypeChecker creates a new type checker.
 func NewTypeChecker(opts ...TypeCheckerOpt) *TypeChecker {
-	tc := &TypeChecker{}
+	tc := &TypeChecker{
+		cache: map[uint64]*TypeInfo{},
+	}
 
 	for _, o := range opts {
 		o(tc)
@@ -114,20 +294,11 @@ func NewTypeChecker(opts ...TypeCheckerOpt) *TypeChecker {
 	return tc
 }
 
-// Check type-checks the given script.
-//
-// It assumes the expression is a list of expressions.
-func (tc *TypeChecker) Check(exp SExp) {
-	for head := exp; head != Nil(); head = head.Cdr() {
-		tc.Type(head.Car())
-	}
-}
-
 func (tc *TypeChecker) error(exp SExp, err error) {
 	tc.errHandler(WrapError(err, exp.Meta(), ""))
 }
 
-func (tc *TypeChecker) typeError(exp SExp, got, want Type) {
+func (tc *TypeChecker) typeError(exp SExp, got, want *TypeInfo) {
 	err := TypeError{
 		Line:   exp.Meta().Line,
 		Offset: exp.Meta().Offset,
@@ -135,104 +306,354 @@ func (tc *TypeChecker) typeError(exp SExp, got, want Type) {
 		Want:   want,
 	}
 
-	tc.errHandler(err)
+	tc.errHandler(errors.WithStack(err))
 }
 
-// Type returns the type of an S-Expression.
-func (tc *TypeChecker) Type(exp SExp) *TypeInfo {
+func (tc *TypeChecker) save(exp SExp, t *TypeInfo) *TypeInfo {
+	tc.cache[exp.ID()] = t
+	return t
+}
+
+// BodyType returns the type info of a body of expressions, emitting type
+// errors to the error handler along the way.
+func (tc *TypeChecker) BodyType(exp SExp) *TypeInfo {
+	var ti *TypeInfo
+
+	ctx := NewTypeCheckerContext(nil)
+
+	if IsList(exp) {
+		if !IsList(exp.Car()) {
+			return tc.check(ctx, exp)
+		}
+	} else {
+		return tc.check(ctx, exp)
+	}
+
+	for head := exp; head != Nil(); head = head.Cdr() {
+		ti = tc.check(ctx, head.Car())
+	}
+
+	return ti
+}
+
+func (tc *TypeChecker) check(ctx *TypeCheckerContext, exp SExp) *TypeInfo {
+	if exp.IsNil() {
+		return &TypeInfo{Type: TypeNil}
+	}
+
+	if symbol, ok := exp.SymbolVal(); ok {
+		v, ok := ctx.Get(symbol)
+		if !ok {
+			tc.error(exp, errors.Wrap(ErrNotBound, symbol))
+			return &TypeInfo{Type: TypeInvalid}
+		}
+
+		return v
+	}
+
 	if exp.IsAtom() {
 		return &TypeInfo{Type: exp.UnderlyingType()}
 	}
 
-	sym, ok := exp.Car().SymbolVal()
+	symbol, ok := exp.Car().SymbolVal()
 	if !ok {
 		tc.error(exp, ErrInvalidCall)
 		return &TypeInfo{Type: TypeInvalid}
 	}
 
-	switch sym {
+	if ti, ok := tc.cache[exp.ID()]; ok {
+		return ti
+	}
+
+	switch symbol {
 	case "=":
-		return &TypeInfo{Type: tc.checkSameTimes(exp, exp.Cdr(), 1, 0)}
+		tc.checkSameTimes(ctx, exp, exp.Cdr(), 1, 0)
+		return tc.save(exp, &TypeInfo{Type: TypeBool})
+
 	case "+", "-", "*", "/", "mod":
-		tc.checkTimes(exp, exp.Cdr(), TypeInt64, 1, 0)
-		return &TypeInfo{Type: TypeInt64}
+		tc.checkTimes(ctx, exp, exp.Cdr(), &TypeInfo{Type: TypeInt64}, 1, 0)
+		return tc.save(exp, &TypeInfo{Type: TypeInt64})
+
 	case "<", ">", "<=", ">=":
-		tc.checkTimes(exp, exp.Cdr(), TypeInt64, 1, 0)
-		return &TypeInfo{Type: TypeBool}
+		tc.checkTimes(ctx, exp, exp.Cdr(), &TypeInfo{Type: TypeInt64}, 1, 0)
+		return tc.save(exp, &TypeInfo{Type: TypeBool})
+
 	case "not":
-		tc.checkTimes(exp, exp.Cdr(), TypeBool, 1, 1)
-		return &TypeInfo{Type: TypeBool}
+		tc.checkTimes(ctx, exp, exp.Cdr(), &TypeInfo{Type: TypeBool}, 1, 1)
+		return tc.save(exp, &TypeInfo{Type: TypeBool})
+
 	case "and", "or":
-		tc.checkTimes(exp, exp.Cdr(), TypeBool, 1, 0)
-		return &TypeInfo{Type: TypeBool}
+		tc.checkTimes(ctx, exp, exp.Cdr(), &TypeInfo{Type: TypeBool}, 1, 0)
+		return tc.save(exp, &TypeInfo{Type: TypeBool})
+
+	case "cons":
+		return tc.save(exp, tc.checkCons(ctx, exp))
+
+	case "car":
+		return tc.save(exp, tc.checkCar(ctx, exp))
+
+	case "cdr":
+		return tc.save(exp, tc.checkCdr(ctx, exp))
+
+	case "let":
+		return tc.save(exp, tc.checkLet(ctx, exp))
+
 	}
 
-	tc.error(exp, ErrUnknownFunc)
+	tc.error(exp, errors.Wrap(ErrUnknownFunc, symbol))
 
-	return &TypeInfo{Type: TypeInvalid}
+	return tc.save(exp, &TypeInfo{Type: TypeInvalid})
 }
 
-func (tc *TypeChecker) checkTimes(parent SExp, exp SExp, typ Type, min, max int) {
-	i, head := 0, exp
+// checkTimes checks that arguments are compatible with the given type.
+//
+// If min is zero, there is no restriction on the minimum number or arguments.
+// If max is zero, there is no restriction on the maximum number or arguments.
+func (tc *TypeChecker) checkTimes(
+	ctx *TypeCheckerContext,
+	parent SExp, exp SExp,
+	ti *TypeInfo,
+	min, max int,
+) {
+	head := exp
+	i := 0
 
 	for ; head != Nil(); head = head.Cdr() {
 		if max != 0 && i >= max {
 			break
 		}
 
-		t := tc.Type(head.Car())
+		t := tc.check(ctx, head.Car())
 		if t.Type == TypeInvalid {
+			i++
 			continue
 		}
 
-		if t.Type != typ {
-			tc.typeError(head.Car(), t.Type, typ)
+		if !ti.Compatible(t) {
+			tc.typeError(head.Car(), t, ti)
 		}
 
 		i++
 	}
 
 	if min != 0 && i < min {
-		tc.error(parent, ErrTypeMissingArg)
+		tc.error(parent, errors.WithStack(ErrTypeMissingArg))
 	}
 
 	if !head.Car().IsNil() {
-		tc.error(head.Car(), ErrTypeExtraArg)
+		tc.error(head.Car(), errors.WithStack(ErrTypeExtraArg))
 	}
 }
 
-func (tc *TypeChecker) checkSameTimes(parent SExp, exp SExp, min, max int) Type {
-	typ := TypeInvalid
-	i, head := 0, exp
+// checkTimes checks that arguments have the same type.
+//
+// If min is zero, there is no restriction on the minimum number or arguments.
+// If max is zero, there is no restriction on the maximum number or arguments.
+// It returns the most specific type found.
+func (tc *TypeChecker) checkSameTimes(
+	ctx *TypeCheckerContext,
+	parent SExp, exp SExp,
+	min, max int,
+) *TypeInfo {
+	ti := &TypeInfo{Type: TypeInvalid}
+	head := exp
+	i := 0
 
 	for ; head != Nil(); head = head.Cdr() {
 		if max != 0 && i >= max {
 			break
 		}
 
-		t := tc.Type(head.Car())
+		t := tc.check(ctx, head.Car())
 		if t.Type == TypeInvalid {
+			i++
 			continue
 		}
 
-		if typ == TypeInvalid {
-			typ = t.Type
+		if ti.Type == TypeInvalid {
+			ti = t
 		}
 
-		if t.Type != typ {
-			tc.typeError(head.Car(), t.Type, typ)
+		if !ti.Compatible(t) {
+			if t.Compatible(ti) {
+				ti = t
+			} else {
+				tc.typeError(head.Car(), t, ti)
+			}
 		}
 
 		i++
 	}
 
 	if min != 0 && i < min {
-		tc.error(parent, ErrTypeMissingArg)
+		tc.error(parent, errors.WithStack(ErrTypeMissingArg))
 	}
 
 	if !head.Car().IsNil() {
-		tc.error(head.Car(), ErrTypeExtraArg)
+		tc.error(head.Car(), errors.WithStack(ErrTypeExtraArg))
 	}
 
-	return typ
+	return ti
+}
+
+func (tc *TypeChecker) checkCons(ctx *TypeCheckerContext, exp SExp) *TypeInfo {
+	cdr := exp.Cdr()
+	if cdr.IsNil() {
+		tc.error(exp, errors.WithStack(ErrTypeMissingArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	cddr := cdr.Cdr()
+	if cddr.IsNil() {
+		tc.error(exp, errors.WithStack(ErrTypeMissingArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if !cddr.Cdr().IsNil() {
+		tc.error(cddr.Cdr(), errors.WithStack(ErrTypeExtraArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	cadr, caddr := cdr.Car(), cddr.Car()
+	lti, rti := tc.check(ctx, cadr), tc.check(ctx, caddr)
+
+	if lti.Type == TypeInvalid || rti.Type == TypeInvalid {
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	ti := &TypeInfo{Type: TypeCell}
+
+	if lti.Type == TypeNil {
+		tc.error(cdr, errors.WithStack(ErrCarNil))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if rti.Type != TypeNil && !rti.IsList {
+		ti.CarType = lti
+		ti.CdrType = rti
+
+		return ti
+	}
+
+	ti.IsList = true
+
+	if rti.Type != TypeNil && !lti.Equals(rti.CarType) {
+		ti.CarType = &TypeInfo{Type: TypeAny}
+	} else {
+		ti.CarType = lti
+	}
+
+	return ti
+}
+
+func (tc *TypeChecker) checkCar(ctx *TypeCheckerContext, exp SExp) *TypeInfo {
+	cdr := exp.Cdr()
+	if cdr.IsNil() {
+		tc.error(exp, errors.WithStack(ErrTypeMissingArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if !cdr.Cdr().IsNil() {
+		tc.error(cdr.Cdr(), errors.WithStack(ErrTypeExtraArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	ti := tc.check(ctx, cdr.Car())
+
+	if ti.Type == TypeInvalid {
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if ti.Type == TypeNil {
+		return &TypeInfo{Type: TypeNil}
+	}
+
+	if ti.Type != TypeCell {
+		tc.error(cdr, errors.WithStack(ErrNotCell))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	return ti.CarType
+}
+
+func (tc *TypeChecker) checkCdr(ctx *TypeCheckerContext, exp SExp) *TypeInfo {
+	cdr := exp.Cdr()
+	if cdr.IsNil() {
+		tc.error(exp, errors.WithStack(ErrTypeMissingArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if !cdr.Cdr().IsNil() {
+		tc.error(cdr.Cdr(), errors.WithStack(ErrTypeExtraArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	ti := tc.check(ctx, cdr.Car())
+
+	if ti.Type == TypeInvalid {
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if ti.Type == TypeNil {
+		return &TypeInfo{Type: TypeNil}
+	}
+
+	if ti.Type != TypeCell {
+		tc.error(cdr, errors.WithStack(ErrNotCell))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if ti.IsList {
+		return ti.CarType
+	}
+
+	return ti.CdrType
+}
+
+func (tc *TypeChecker) checkLet(ctx *TypeCheckerContext, exp SExp) *TypeInfo {
+	cdr := exp.Cdr()
+	if cdr.IsNil() {
+		tc.error(exp, errors.WithStack(ErrTypeMissingArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	cddr := cdr.Cdr()
+	if cddr.IsNil() {
+		tc.error(exp, errors.WithStack(ErrTypeMissingArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if !cddr.Cdr().IsNil() {
+		tc.error(cddr.Cdr(), errors.WithStack(ErrTypeExtraArg))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	cadr := cdr.Car()
+
+	if cadr.UnderlyingType() != TypeSymbol {
+		tc.error(cdr, errors.WithStack(ErrNotSymbol))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	caddr := cddr.Car()
+	ti := tc.check(ctx, caddr)
+
+	if ti.Type == TypeInvalid {
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	if ti.Type == TypeNil {
+		tc.error(cddr, errors.WithStack(ErrNil))
+		return &TypeInfo{Type: TypeInvalid}
+	}
+
+	symbol := cadr.MustSymbolVal()
+
+	if ctx.IsLocal(symbol) {
+		tc.error(cadr, errors.Wrap(ErrBound, symbol))
+	}
+
+	ctx.Set(symbol, ti)
+
+	return ti
 }

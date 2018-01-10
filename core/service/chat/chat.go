23b0d86b2c92e63ff4565a845333057078e612c6
t-bast <bastien@stratumn.com>
2018-01-10 10:52:05 +0100

    chat: create empty service

---
+++ core/service/chat/chat.go
@@ -0,0 +1,134 @@
+// Copyright © 2017-2018 Stratumn SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package chat is a simple service that allows two peers to exchange messages
+package chat
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	ihost "gx/ipfs/QmP46LGWhzVZTMmt5akNNLfoV8qL4h5wTwmzQxLyDafggd/go-libp2p-host"
+	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+)
+
+var (
+	// ErrNotHost is returned when the connected service is not a host.
+	ErrNotHost = errors.New("connected service is not a host")
+
+	// ErrUnavailable is returned from gRPC methods when the service is not
+	// available.
+	ErrUnavailable = errors.New("the service is not available")
+)
+
+// Host represents an Alice host.
+type Host = ihost.Host
+
+// log is the logger for the service.
+var log = logging.Logger("chat")
+
+// Service is the Chat service.
+type Service struct {
+	config *Config
+	host   Host
+}
+
+// Config contains configuration options for the Chat service.
+type Config struct {
+	// Host is the name of the host service.
+	Host string `toml:"host" comment:"The name of the host service."`
+}
+
+// ID returns the unique identifier of the service.
+func (s *Service) ID() string {
+	return "chat"
+}
+
+// Name returns the human friendly name of the service.
+func (s *Service) Name() string {
+	return "Chat"
+}
+
+// Desc returns a description of what the service does.
+func (s *Service) Desc() string {
+	return "A basic p2p chat service."
+}
+
+// Config returns the current service configuration or creates one with
+// good default values.
+func (s *Service) Config() interface{} {
+	if s.config != nil {
+		return *s.config
+	}
+
+	// Set the default configuration settings of your service here.
+	return Config{
+		Host: "host",
+	}
+}
+
+// SetConfig configures the service handler.
+func (s *Service) SetConfig(config interface{}) error {
+	conf := config.(Config)
+	s.config = &conf
+	return nil
+}
+
+// Needs returns the set of services this service depends on.
+func (s *Service) Needs() map[string]struct{} {
+	needs := map[string]struct{}{}
+	needs[s.config.Host] = struct{}{}
+
+	return needs
+}
+
+// Plug sets the connected services.
+func (s *Service) Plug(services map[string]interface{}) error {
+	var ok bool
+
+	if s.host, ok = services[s.config.Host].(Host); !ok {
+		return errors.Wrap(ErrNotHost, s.config.Host)
+	}
+
+	return nil
+}
+
+// Expose exposes the chat service to other services.
+func (s *Service) Expose() interface{} {
+	// TODO
+	return nil
+}
+
+// Run starts the service.
+func (s *Service) Run(ctx context.Context, running, stopping func()) error {
+
+	// Initialize the service before calling running().
+
+	running()
+
+	// Start any long running process with a goroutine here.
+
+	// Handle exit conditions.
+	select {
+	case <-ctx.Done():
+		// ...
+	}
+
+	stopping()
+
+	// Stop the service after calling stopping().
+
+	return errors.WithStack(ctx.Err())
+}

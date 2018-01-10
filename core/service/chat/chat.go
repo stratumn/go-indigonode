8de02cdd284077cd0accca3cbabbde1890ad0d79
t-bast <bastien@stratumn.com>
2018-01-10 16:47:38 +0100

    chat: add missing dot in godoc

--- core/service/chat/chat.go
+++ core/service/chat/chat.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package chat is a simple service that allows two peers to exchange messages
+// Package chat is a simple service that allows two peers to exchange messages.
 package chat
 
 import (

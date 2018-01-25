093638d071f733620eb72b3ea0b30263b9ab00ce
t-bast <bastien@stratumn.com>
2018-01-25 17:47:34 +0100

    coin: generate mocks

--- core/protocol/coin/chain.go
+++ core/protocol/coin/chain.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate mockgen -package mockcoin -destination mockcoin/mockchainreader.go github.com/stratumn/alice/core/protocol/coin ChainReader
+
 package coin
 
 import (
--- core/protocol/coin/engine.go
+++ core/protocol/coin/engine.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate mockgen -package mockcoin -destination mockcoin/mockengine.go github.com/stratumn/alice/core/protocol/coin Engine
+
 package coin
 
 import (

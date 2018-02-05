92f4caa6c23953689d9ee1768a0176370d4d53cc
t-bast <bastien@stratumn.com>
2018-02-05 11:23:59 +0100

    chain: simplechain testutil

--- core/protocol/coin/testutil/chain.go
+++ core/protocol/coin/testutil/chain.go
@@ -15,17 +15,82 @@
 package testutil
 
 import (
-	"testing"
+	"bytes"
+	"sync"
 
 	"github.com/stratumn/alice/core/protocol/coin/chain"
-	db "github.com/stratumn/alice/core/protocol/coin/db"
-	"github.com/stretchr/testify/require"
+	"github.com/stratumn/alice/core/protocol/coin/coinutils"
+	pb "github.com/stratumn/alice/pb/coin"
 )
 
-// NewSimpleChain returns a SimpleState ready to use in tests.
-func NewSimpleChain(t *testing.T, opts ...chain.Opt) chain.Chain {
-	memdb, err := db.NewMemDB(nil)
-	require.NoError(t, err, "db.NewMemDB()")
+// SimpleChain can be used in tests to inject arbitrary blocks.
+type SimpleChain struct {
+	mu           sync.RWMutex
+	blocks       []*pb.Block
+	currentBlock *pb.Block
+}
 
-	return chain.NewChainDB(memdb, opts...)
+// Config returns nothing.
+func (c *SimpleChain) Config() *chain.Config {
+	return nil
+}
+
+// CurrentHeader returns the header of the last block added.
+func (c *SimpleChain) CurrentHeader() *pb.Header {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	return c.currentBlock.Header
+}
+
+// GetHeaderByNumber returns all headers that have the input BlockNumber.
+func (c *SimpleChain) GetHeaderByNumber(number uint64) []*pb.Header {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var res []*pb.Header
+	for _, b := range c.blocks {
+		if b.Header.BlockNumber == number {
+			res = append(res, b.Header)
+		}
+	}
+
+	return res
+}
+
+// GetHeaderByHash returns the first header of the block with the given hash.
+func (c *SimpleChain) GetHeaderByHash(hash []byte) *pb.Header {
+	b := c.GetBlock(hash, 0)
+	return b.Header
+}
+
+// GetBlock returns the first block with the given hash.
+func (c *SimpleChain) GetBlock(hash []byte, _ uint64) *pb.Block {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	for _, b := range c.blocks {
+		blockHash, err := coinutils.HashBlock(b)
+		if err != nil {
+			return nil
+		}
+
+		if bytes.Equal(hash, blockHash) {
+			return b
+		}
+	}
+
+	return nil
+}
+
+// AddBlock adds a block to the chain without any validation.
+// It also sets the given block as current block.
+func (c *SimpleChain) AddBlock(block *pb.Block) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.blocks = append(c.blocks, block)
+	c.currentBlock = block
+
+	return nil
 }

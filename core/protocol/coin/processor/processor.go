6cda69e335bd2d408973869ce168e79399a66812
Mourad BEJI <mourad.beji@stratumn.com>
2018-02-07 11:25:13 +0100

    coin/processor: reorgs + multihash

--- core/protocol/coin/processor/processor.go
+++ core/protocol/coin/processor/processor.go
@@ -17,6 +17,8 @@
 package processor
 
 import (
+	"bytes"
+
 	"github.com/stratumn/alice/core/protocol/coin/chain"
 	"github.com/stratumn/alice/core/protocol/coin/coinutil"
 	"github.com/stratumn/alice/core/protocol/coin/state"
@@ -27,34 +29,119 @@ import (
 type Processor interface {
 	// Process applies the state changes from the block contents
 	// and adds the block to the chain.
-	Process(block *pb.Block, state state.State, chain chain.Writer) error
+	Process(block *pb.Block, state state.State, chain chain.Chain) error
 }
 
 type processor struct{}
 
 // NewProcessor creates a new processor.
 func NewProcessor() Processor {
-	return processor{}
+	return &processor{}
 }
 
-func (processor) Process(block *pb.Block, state state.State, chain chain.Writer) error {
-	// TODO: miner reward.
+type stateTransition struct {
+	stateID      []byte
+	transactions []*pb.Transaction
+}
 
+func (p *processor) Process(block *pb.Block, state state.State, chain chain.Chain) error {
 	// Update chain.
 	if err := chain.AddBlock(block); err != nil {
 		return err
 	}
-	if err := chain.SetHead(block); err != nil {
-		return err
-	}
 
-	// Update state.
-	// TODO: instead of recomputing the hash here,
-	// we can have chain.AddBlock return it.
-	h, err := coinutil.HashHeader(block.Header)
+	head, err := chain.CurrentBlock()
 	if err != nil {
 		return err
 	}
 
-	return state.ProcessTransactions(h[:], block.Transactions)
+	// If the block is not higher than the current head,
+	// do not update state, end processing.
+	if head != nil && head.Header.BlockNumber > block.Header.BlockNumber {
+		return nil
+	}
+
+	// Set the new head.
+	if err := chain.SetHead(block); err != nil {
+		return err
+	}
+
+	// If the new head is on a different branch, reorganize state.
+	if head != nil {
+		hash, err := coinutil.HashHeader(head.Header)
+		if err != nil {
+			return err
+		}
+		if !bytes.Equal(hash, block.Header.PreviousHash) {
+			return p.reorg(head, block, state, chain)
+		}
+	}
+
+	// Update state.
+	h, err := coinutil.HashHeader(block.Header)
+	if err != nil {
+		return err
+	}
+	return state.ProcessTransactions(h, block.Transactions)
+}
+
+// Update the state to follow the new main branch.
+func (p *processor) reorg(prevHead *pb.Block, newHead *pb.Block, state state.State, chain chain.Chain) error {
+	backward := []*stateTransition{}
+	forward := []*stateTransition{}
+
+	mainBlock := newHead
+	mainNumber := mainBlock.Header.BlockNumber
+	mainHash, err := coinutil.HashHeader(mainBlock.Header)
+	if err != nil {
+		return err
+	}
+
+	forkBlock := prevHead
+	forkNumber := forkBlock.Header.BlockNumber
+	forkHash, err := coinutil.HashHeader(forkBlock.Header)
+	if err != nil {
+		return err
+	}
+
+	// First gets the two chains to the same height.
+	// Then go back to first common ancestor.
+	for !bytes.Equal(mainHash, forkHash) {
+		if mainNumber >= forkNumber {
+			forward = append(forward, &stateTransition{stateID: mainHash, transactions: mainBlock.Transactions})
+			mainHash = mainBlock.Header.PreviousHash
+			mainBlock, err = chain.GetBlock(mainBlock.Header.PreviousHash, mainBlock.Header.BlockNumber-1)
+			if err != nil {
+				return err
+			}
+		}
+
+		if mainNumber <= forkNumber {
+			// Prepend transition to backward to be then played in good order.
+			backward = append(
+				[]*stateTransition{&stateTransition{stateID: forkHash, transactions: forkBlock.Transactions}},
+				backward...,
+			)
+			forkHash = forkBlock.Header.PreviousHash
+			forkBlock, err = chain.GetBlock(forkBlock.Header.PreviousHash, forkBlock.Header.BlockNumber-1)
+			if err != nil {
+				return err
+			}
+		}
+		mainNumber = mainBlock.Header.BlockNumber
+		forkNumber = forkBlock.Header.BlockNumber
+	}
+
+	for _, st := range backward {
+		if err = state.RollbackTransactions(st.stateID, st.transactions); err != nil {
+			return err
+		}
+	}
+
+	for _, st := range forward {
+		if err = state.ProcessTransactions(st.stateID, st.transactions); err != nil {
+			return err
+		}
+	}
+	return nil
 }

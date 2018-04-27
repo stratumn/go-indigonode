20df75841c310a665f6b30367f2c9c7346e4841a
Mourad BEJI <mourad.beji@stratumn.com>
2018-04-27 13:44:43 +0200

    storage: refacto file handler

--- core/protocol/storage/file/handler.go
+++ core/protocol/storage/file/handler.go
@@ -18,13 +18,20 @@ package file
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
+	"github.com/satori/go.uuid"
+	"github.com/stratumn/alice/core/db"
 	pb "github.com/stratumn/alice/pb/storage"
 
 	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	mh "gx/ipfs/QmZyZDi491cCNTLfAhwcaDii2Kg4pwKRkhqQzURGDvY6ua/go-multihash"
 )
 
 var log = logging.Logger("storage.file_handler")
@@ -33,24 +40,60 @@ var (
 	// ErrFileNameMissing is returned when no file name was given.
 	ErrFileNameMissing = errors.New("the first chunk should have the filename")
 
+	// ErrFileNameMissing is returned when no file name was given.
+	ErrNoSession = errors.New("no open file session with this id")
+
 	// ErrUnauthorized is returned when a peer tries to access a file he
 	// is not allowed to get
 	ErrUnauthorized = errors.New("peer not authorized for requested file")
 )
 
+var (
+	prefixFilesHashes = []byte("fh") // prefixFilesHashes + filehash -> filepath
+)
+
 // Handler contains the methods to handle a file on the alice node.
 type Handler interface {
 	WriteFile(context.Context, <-chan *pb.FileChunk) (*os.File, error)
+
+	// BeginWrite creates an empty file.
+	BeginWrite(ctx context.Context, fileName string) (uuid.UUID, error)
+
+	// WriteChunk writes a chunk of data to a file identified by its ID.
+	WriteChunk(ctx context.Context, sessionID uuid.UUID, chunk []byte) (err error)
+
+	// EndWrite is called to finilize the file writing.
+	EndWrite(ctx context.Context, sessionID uuid.UUID) (fileHash []byte, err error)
+}
+
+// session represents one file write.
+type session struct {
+	id   uuid.UUID
+	file *os.File
+}
+
+func newSession(file *os.File) *session {
+	id := uuid.NewV4()
+	return &session{
+		id:   id,
+		file: file,
+	}
 }
 
 type localFileHandler struct {
-	storagePath string
+	db            db.DB
+	writeSessions map[uuid.UUID]*session
+	readSessions  map[uuid.UUID]*session
+	storagePath   string
 }
 
 // NewLocalFileHandler create a new file Handler.
-func NewLocalFileHandler(path string) Handler {
+func NewLocalFileHandler(path string, db db.DB) Handler {
 	return &localFileHandler{
-		storagePath: path,
+		db:            db,
+		storagePath:   path,
+		writeSessions: make(map[uuid.UUID]*session),
+		readSessions:  make(map[uuid.UUID]*session),
 	}
 }
 
@@ -99,4 +142,224 @@ func (h *localFileHandler) WriteFile(ctx context.Context, chunkCh <-chan *pb.Fil
 			}
 		}
 	}
+}
+
+// ============================================================================
+// ====															Sequential write											 ====
+// ============================================================================
+
+// BeginWrite creates an empty file and attaches it to a session.
+func (h *localFileHandler) BeginWrite(ctx context.Context, fileName string) (uuid.UUID, error) {
+	event := log.EventBegin(ctx, "BeginWrite", &logging.Metadata{"fileName": fileName})
+	defer event.Done()
+
+	file, err := os.Create(filepath.Join(h.storagePath, fileName))
+	if err != nil {
+		event.SetError(err)
+		return uuid.Nil, errors.WithStack(err)
+	}
+
+	session := newSession(file)
+	h.writeSessions[session.id] = session
+	event.Append(&logging.Metadata{"sessionID": session.id})
+
+	return session.id, nil
+}
+
+// WriteChunk writes a chunk of data to a file identified by its session ID.
+func (h *localFileHandler) WriteChunk(ctx context.Context, sessionID uuid.UUID, chunk []byte) (err error) {
+	// TODO: do we really want to log this ? Feels like good spam here...
+	event := log.EventBegin(ctx, "WriteChunk", &logging.Metadata{"sessionID": sessionID})
+	defer func() {
+		if err != nil {
+			event.SetError(err)
+			// TODO: this prevents us from doing retry at chunk level...
+			h.deleteFile(ctx, sessionID)
+		}
+		event.Done()
+	}()
+
+	session, ok := h.writeSessions[sessionID]
+	if !ok {
+		err = ErrNoSession
+		return
+	}
+
+	_, err = session.file.Write(chunk)
+	if err != nil {
+	}
+	return
+}
+
+// EndWrite must be called at the end of the writing process.
+// It indexes the file, cleans the session and returns the filehash.
+func (h *localFileHandler) EndWrite(ctx context.Context, sessionID uuid.UUID) (fileHash []byte, err error) {
+	event := log.EventBegin(ctx, "EndWrite", &logging.Metadata{"sessionID": sessionID})
+	defer func() {
+		if err != nil {
+			event.SetError(err)
+			h.deleteFile(ctx, sessionID)
+		}
+		event.Done()
+	}()
+
+	session, ok := h.writeSessions[sessionID]
+	if !ok {
+		err = ErrNoSession
+		return
+	}
+
+	fileHash, err = h.indexFile(ctx, session.file)
+	if err != nil {
+		return
+	}
+
+	delete(h.writeSessions, sessionID)
+	if err = session.file.Close(); err != nil {
+		event.Append(&logging.Metadata{"closeFileError": err.Error()})
+	}
+
+	return
+}
+
+// DeleteFile deletes a file and it's session.
+// Used to clean partially written files when an error occurs.
+func (h *localFileHandler) deleteFile(ctx context.Context, sessionID uuid.UUID) (err error) {
+	event := log.EventBegin(ctx, "DeleteFile", &logging.Metadata{"sessionID": sessionID})
+	defer func() {
+		if err != nil {
+			event.SetError(err)
+		}
+		event.Done()
+	}()
+
+	session, ok := h.writeSessions[sessionID]
+	if !ok {
+		err = ErrNoSession
+		return
+	}
+
+	if err = session.file.Close(); err != nil {
+		event.Append(&logging.Metadata{"closeFileError": err.Error()})
+	}
+
+	err = os.Remove(session.file.Name())
+	if err != nil {
+		return
+	}
+
+	delete(h.writeSessions, sessionID)
+	return
+}
+
+// ============================================================================
+// ====															Sequential read											 	 ====
+// ============================================================================
+
+// BeginRead opens a file given its hash and attaches it to a session.
+func (h *localFileHandler) BeginRead(ctx context.Context, fileHash []byte) (uuid.UUID, error) {
+	event := log.EventBegin(ctx, "BeginRead", &logging.Metadata{"fileHash": hex.EncodeToString(fileHash)})
+	defer event.Done()
+
+	filePath, err := h.getFilePath(ctx, fileHash)
+	if err != nil {
+		event.SetError(err)
+		return uuid.Nil, err
+	}
+
+	file, err := os.Open(filepath.Join(h.storagePath, filePath))
+	if err != nil {
+		event.SetError(err)
+		return uuid.Nil, errors.WithStack(err)
+	}
+
+	session := newSession(file)
+	h.readSessions[session.id] = session
+	event.Append(&logging.Metadata{"sessionID": session.id})
+
+	return session.id, nil
+}
+
+// ReadChunk reads a chunk of data.
+func (h *localFileHandler) ReadChunk(ctx context.Context, sessionID uuid.UUID, chunkSize int) ([]byte, error) {
+	// TODO: do we really want to log this ? Feels like good spam here...
+	event := log.EventBegin(ctx, "ReadChunk", &logging.Metadata{"sessionID": sessionID})
+	defer event.Done()
+
+	session, ok := h.readSessions[sessionID]
+	if !ok {
+		event.SetError(ErrNoSession)
+		return nil, ErrNoSession
+	}
+
+	chunk := make([]byte, chunkSize)
+	n, err := session.file.Read(chunk)
+	if err != nil {
+		if err != io.EOF {
+			event.SetError(err)
+			// TODO: deleteing the session here means that we won't be able to retry.
+			delete(h.readSessions, sessionID)
+			if err = session.file.Close(); err != nil {
+				event.Append(&logging.Metadata{"closeFileError": err.Error()})
+			}
+		}
+		return nil, err
+	}
+
+	return chunk[:n], nil
+}
+
+// EndRead must be called at the end of the read process to delete the session.
+func (h *localFileHandler) EndRead(ctx context.Context, sessionID uuid.UUID) error {
+	event := log.EventBegin(ctx, "EndRead", &logging.Metadata{"sessionID": sessionID})
+
+	session, ok := h.readSessions[sessionID]
+	if !ok {
+		event.SetError(ErrNoSession)
+		return ErrNoSession
+	}
+
+	delete(h.readSessions, sessionID)
+	if err := session.file.Close(); err != nil {
+		event.Append(&logging.Metadata{"closeFileError": err.Error()})
+	}
+	return nil
+}
+
+// ============================================================================
+// ====															indexing														 	 ====
+// ============================================================================
+
+// indexFile adds the file hash and name to the db.
+func (h *localFileHandler) indexFile(ctx context.Context, file *os.File) ([]byte, error) {
+	// go back to the beginning of the file.
+	if _, err := file.Seek(0, 0); err != nil {
+		return nil, err
+	}
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, err
+	}
+
+	fileHash, err := mh.Encode(hash.Sum(nil), mh.SHA2_256)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = h.db.Put(append(prefixFilesHashes, fileHash...), []byte(file.Name())); err != nil {
+		return nil, err
+	}
+
+	return fileHash, nil
+}
+
+// getFilePath returns the file path given its hash.
+func (h *localFileHandler) getFilePath(ctx context.Context, fileHash []byte) (string, error) {
+	p, err := h.db.Get(append(prefixFilesHashes, fileHash...))
+	if err != nil {
+		return "", err
+	}
+
+	return string(p), nil
 }
--- core/protocol/storage/file/handler_test.go
+++ core/protocol/storage/file/handler_test.go
@@ -16,11 +16,18 @@ package file
 
 import (
 	"context"
+	"crypto/sha256"
 	"fmt"
+	mh "gx/ipfs/QmZyZDi491cCNTLfAhwcaDii2Kg4pwKRkhqQzURGDvY6ua/go-multihash"
+	"io"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stratumn/alice/core/db"
 	pb "github.com/stratumn/alice/pb/storage"
 	"github.com/stretchr/testify/assert"
 )
@@ -28,8 +35,10 @@ import (
 func TestFileHandler_Write(t *testing.T) {
 
 	tmpPath := "/tmp"
+	db, err := db.NewMemDB(nil)
+	require.NoError(t, err, "NewMemDB()")
 
-	fileHandler := NewLocalFileHandler(tmpPath)
+	fileHandler := NewLocalFileHandler(tmpPath, db)
 
 	t.Run("successfully-write", func(t *testing.T) {
 		ctx := context.Background()
@@ -163,4 +172,320 @@ func TestFileHandler_Write(t *testing.T) {
 		<-doneCh
 	})
 
+}
+
+// ============================================================================
+// ==== 															Write																 ====
+// ============================================================================
+
+func TestFileHandler_BeginWrite(t *testing.T) {
+
+	fileHandler := &localFileHandler{
+		storagePath:   "/tmp",
+		writeSessions: make(map[uuid.UUID]*session),
+	}
+
+	fileName := fmt.Sprintf("TestFileHandler_BeginWrite-%d", time.Now().UnixNano())
+	id, err := fileHandler.BeginWrite(context.Background(), fileName)
+	assert.NoError(t, err, "BeginWrite")
+
+	// Check that the session and the file have been created.
+	session, ok := fileHandler.writeSessions[id]
+	require.True(t, ok, "fileHandler.writeSessions[id]")
+	require.NotNil(t, session.file, "session.file")
+
+	assert.Equal(t, "/tmp/"+fileName, session.file.Name())
+
+	err = os.Remove("/tmp/" + fileName)
+	assert.NoError(t, err, "os.Remove")
+}
+
+func TestFileHandler_WriteChunk(t *testing.T) {
+
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_WriteChunk-%d", time.Now().UnixNano())
+
+	file, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+
+	_, err = file.Write([]byte("I love to write"))
+	assert.NoError(t, err, "os.Write")
+
+	sess := newSession(file)
+
+	fileHandler := &localFileHandler{
+		storagePath:   "/tmp",
+		writeSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	chunk := []byte(" some data")
+	err = fileHandler.WriteChunk(context.Background(), sess.id, chunk)
+	assert.NoError(t, err, "WriteChunk")
+
+	// Check that session is still open and that chunk was appended to data .
+	session, ok := fileHandler.writeSessions[sess.id]
+	require.True(t, ok, "fileHandler.writeSessions[id]")
+	require.NotNil(t, session.file, "session.file")
+
+	data := make([]byte, 100)
+	session.file.Seek(0, 0)
+	n, err := session.file.Read(data)
+	assert.NoError(t, err, "session.file.Read()")
+	assert.Equal(t, []byte("I love to write some data"), data[:n], "session.file.Read()")
+
+	err = os.Remove(filePath)
+	assert.NoError(t, err, "os.Remove")
+}
+
+func TestFileHandler_WriteChunk_Fail(t *testing.T) {
+
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_WriteChunk_Fail-%d", time.Now().UnixNano())
+
+	file, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+
+	sess := newSession(file)
+
+	fileHandler := &localFileHandler{
+		storagePath:   "/tmp",
+		writeSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	t.Run("no-session", func(t *testing.T) {
+		err = fileHandler.WriteChunk(context.Background(), uuid.NewV4(), []byte(" some data"))
+		assert.EqualError(t, err, ErrNoSession.Error(), "WriteChunk")
+	})
+
+	t.Run("fail-write-and-delete", func(t *testing.T) {
+		err := sess.file.Close()
+		require.NoError(t, err, "session.File.Close()")
+
+		err = fileHandler.WriteChunk(context.Background(), sess.id, []byte("yo"))
+		assert.Error(t, err, "WriteChunk")
+
+		// Check that session has been deleted.
+		_, ok := fileHandler.writeSessions[sess.id]
+		require.False(t, ok, "fileHandler.writeSessions[id]")
+
+		// Check that teh file has been deleted.
+		_, err = os.Stat(sess.file.Name())
+		assert.True(t, os.IsNotExist(err), "File should be deleted")
+	})
+}
+
+func TestFileHandler_EndWrite(t *testing.T) {
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_EndWrite-%d", time.Now().UnixNano())
+
+	file, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+
+	_, err = file.Write([]byte("I love to write"))
+	assert.NoError(t, err, "os.Write")
+
+	sess := newSession(file)
+
+	db, err := db.NewMemDB(nil)
+	require.NoError(t, err, "NewMemDB()")
+
+	fileHandler := &localFileHandler{
+		db:            db,
+		storagePath:   "/tmp",
+		writeSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	hash, err := fileHandler.EndWrite(context.Background(), sess.id)
+	require.NoError(t, err, "EndWrite()")
+
+	// Check that hash is correct.
+	f, err := os.Open(filePath)
+	assert.NoError(t, err)
+
+	h := sha256.New()
+	_, err = io.Copy(h, f)
+	assert.NoError(t, err, "io.Copy")
+
+	expected, err := mh.Encode(h.Sum(nil), mh.SHA2_256)
+	assert.NoError(t, err, "mh.Encode")
+	assert.Equal(t, expected, hash, "file hash incorrect")
+
+	// Check that file hash and path are in db.
+	p, err := db.Get(append(prefixFilesHashes, hash...))
+	assert.NoError(t, err, "db.Get()")
+	assert.Equal(t, filePath, string(p), "incorrect path saved")
+
+	err = os.Remove(filePath)
+	assert.NoError(t, err, "os.Remove")
+}
+
+func TestFileHandler_EndWrite_Fail(t *testing.T) {
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_EndWrite_Fail-%d", time.Now().UnixNano())
+
+	file, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+
+	_, err = file.Write([]byte("I love to write"))
+	assert.NoError(t, err, "os.Write")
+
+	sess := newSession(file)
+
+	db, err := db.NewMemDB(nil)
+	require.NoError(t, err, "NewMemDB()")
+
+	fileHandler := &localFileHandler{
+		db:            db,
+		storagePath:   "/tmp",
+		writeSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	t.Run("no-session", func(t *testing.T) {
+		_, err = fileHandler.EndWrite(context.Background(), uuid.NewV4())
+		assert.EqualError(t, err, ErrNoSession.Error(), "WriteChunk")
+	})
+
+	t.Run("fail-and-delete-file", func(t *testing.T) {
+		err := db.Close()
+		assert.NoError(t, err, "db.Close()")
+
+		_, err = fileHandler.EndWrite(context.Background(), sess.id)
+		assert.Error(t, err, "WriteChunk")
+
+		// Check that session has been deleted.
+		_, ok := fileHandler.writeSessions[sess.id]
+		require.False(t, ok, "fileHandler.writeSessions[id]")
+
+		// Check that teh file has been deleted.
+		_, err = os.Stat(sess.file.Name())
+		assert.True(t, os.IsNotExist(err), "File should be deleted")
+
+	})
+}
+
+// ============================================================================
+// ==== 															Read																 ====
+// ============================================================================
+
+func TestFileHandler_BeginRead(t *testing.T) {
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_BeginRead-%d", time.Now().UnixNano())
+	fileHash := []byte("file hash")
+
+	_, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+	defer os.Remove(filePath)
+
+	db, err := db.NewMemDB(nil)
+	require.NoError(t, err)
+
+	err = db.Put(append(prefixFilesHashes, fileHash...), []byte(filePath))
+	require.NoError(t, err)
+
+	fileHandler := &localFileHandler{
+		db:           db,
+		readSessions: make(map[uuid.UUID]*session),
+	}
+
+	id, err := fileHandler.BeginRead(context.Background(), fileHash)
+	assert.NoError(t, err, "BeginRead")
+
+	// Check that the session has teh right file.
+	session, ok := fileHandler.readSessions[id]
+	require.True(t, ok, "fileHandler.readSessions[id]")
+	require.NotNil(t, session.file, "session.file")
+	assert.Equal(t, filePath, session.file.Name(), "session.file.Name()")
+}
+
+func TestFileHandler_ReadChunk(t *testing.T) {
+
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_ReadChunk-%d", time.Now().UnixNano())
+
+	file, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+	defer os.Remove(filePath)
+
+	data := []byte("The file has some data")
+	_, err = file.Write(data)
+	assert.NoError(t, err, "os.Write")
+
+	_, err = file.Seek(0, 0)
+	assert.NoError(t, err)
+
+	sess := newSession(file)
+	chunkSize := 12
+
+	fileHandler := &localFileHandler{
+		readSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	// Read first chunk.
+	chunk, err := fileHandler.ReadChunk(context.Background(), sess.id, chunkSize)
+	assert.NoError(t, err, "ReadChunk")
+	assert.Equal(t, data[:12], chunk, "first chunk")
+
+	// Check that second chunk is read second.
+	chunk, err = fileHandler.ReadChunk(context.Background(), sess.id, chunkSize)
+	assert.NoError(t, err, "ReadChunk")
+	assert.Equal(t, data[chunkSize:], chunk, "second chunk")
+
+	// Check that we have EOF as there should be no more data to read.
+	_, err = fileHandler.ReadChunk(context.Background(), sess.id, chunkSize)
+	assert.EqualError(t, err, io.EOF.Error(), "ReadChunk")
+}
+
+func TestFileHandler_ReadChunk_Fail(t *testing.T) {
+	filePath := fmt.Sprintf("/tmp/TestFileHandler_ReadChunk-%d", time.Now().UnixNano())
+
+	file, err := os.Create(filePath)
+	require.NoError(t, err, "os.Create")
+	defer os.Remove(filePath)
+
+	sess := newSession(file)
+	chunkSize := 12
+
+	fileHandler := &localFileHandler{
+		readSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	t.Run("no-session", func(t *testing.T) {
+		_, err := fileHandler.ReadChunk(context.Background(), uuid.NewV4(), chunkSize)
+		assert.EqualError(t, err, ErrNoSession.Error(), "ReadChunk")
+	})
+
+	t.Run("fail-and-delete-session", func(t *testing.T) {
+		err = file.Close()
+		assert.NoError(t, err, "file.Close()")
+
+		_, err := fileHandler.ReadChunk(context.Background(), sess.id, chunkSize)
+		assert.Error(t, err, "ReadChunk")
+
+		_, ok := fileHandler.readSessions[sess.id]
+		assert.False(t, ok, "readSessions[id]")
+	})
+}
+
+func TestFileHandler_EndRead(t *testing.T) {
+	fileName := fmt.Sprintf("TestFileHandler_EndRead-%d", time.Now().UnixNano())
+
+	file, err := os.Create("/tmp/" + fileName)
+	require.NoError(t, err, "os.Create")
+
+	sess := newSession(file)
+
+	fileHandler := &localFileHandler{
+		readSessions: map[uuid.UUID]*session{sess.id: sess},
+	}
+
+	err = fileHandler.EndRead(context.Background(), sess.id)
+	require.NoError(t, err, "EndRead()")
+
+	// Check that session was deleted and file closed.
+	_, ok := fileHandler.readSessions[sess.id]
+	assert.False(t, ok, "readSessions[id]")
+}
+
+func TestFileHandler_EndRead_Fail(t *testing.T) {
+
+	fileHandler := &localFileHandler{
+		readSessions: map[uuid.UUID]*session{},
+	}
+
+	err := fileHandler.EndRead(context.Background(), uuid.NewV4())
+	assert.EqualError(t, err, ErrNoSession.Error(), "ReadChunk")
 }

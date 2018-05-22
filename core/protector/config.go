e5ca12395f30ca722f1ef9bcfd7894d52daaadd4
t-bast <bastien@stratumn.com>
2018-05-22 09:52:41 +0200

    protector: add config interface

---
+++ core/protector/config.go
@@ -0,0 +1,27 @@
+// Copyright © 2017-2018 Stratumn SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protector
+
+import (
+	"context"
+
+	"gx/ipfs/QmcJukH2sAFjY3HdBKq35WDzWoL3UUu2gt9wdfqZTUyM74/go-libp2p-peer"
+)
+
+// Config manages the network participants list.
+type Config interface {
+	AddPeer(context.Context, peer.ID) error
+	RemovePeer(context.Context, peer.ID) error
+}
